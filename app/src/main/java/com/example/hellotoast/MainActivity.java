0fe09b4ddea75cc12f17729d29438699e615758b
siddhartha bhattarai <siddharthabhattarai@siddharthas-MBP.domain.name>
2021-03-18 14:19:09 +0545

    task 1 complete

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mShowCount;
+    private int mCount=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUP(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
